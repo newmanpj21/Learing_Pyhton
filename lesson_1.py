6a321881f9d048ca6091876b7172c4fa8bcef22d
newmanpj21 <43393873+newmanpj21@users.noreply.github.com>
2018-10-18 14:13:54 -0400

    Lesson 1

---
+++ lesson_1.py
@@ -0,0 +1,19 @@
+# Python Starter Program
+
+# Define word variables
+word = 'Python'
+word2 = "Python"
+
+# Define number variables
+number = 1
+
+# Print phrases to the console
+print('Python is better than Java')
+
+# Print things using variables
+print(word)
+
+# Create a list 
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+input("press enter to quit")
