30bb423cc064cb4d42e2267bd55c02e5396f4589
newmanpj21 <43393873+newmanpj21@users.noreply.github.com>
2018-10-19 17:41:20 -0400

    addition of lesson 2

--- Lessons/lesson_2.py
+++ Lessons/lesson_2.py
@@ -1,1 +1,14 @@
 """Simple math operators"""
+
+# Get user input and store in variables
+
+val1 = int(input("What is the first value: "))
+val2 = int(input("What is the second value: "))
+
+# add the values
+total = val1 + val2
+thing = 'thing'
+other_thing = 'other thing'
+
+# print the sum out on the screen
+print('The total is %s ' % total)
