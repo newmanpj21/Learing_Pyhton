33f200f24a991e42dcc72d3de2d8bcec995b75d3
newmanpj21 <43393873+newmanpj21@users.noreply.github.com>
2018-10-21 23:00:16 -0400

    lesson 2

--- Lessons/lesson_2.py
+++ Lessons/lesson_2.py
@@ -7,8 +7,10 @@ val2 = int(input("What is the second value: "))
 
 # add the values
 total = val1 + val2
-thing = 'thing'
-other_thing = 'other thing'
+
 
 # print the sum out on the screen
 print('The total is %s ' % total)
+
+# this can also be done this way
+print('The total is', total)
